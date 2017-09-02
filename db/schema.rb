83e7c43727540ffa36921c9dd7d86be8688c2683
devpatcar <patrik.g.carlsson@outook.com>
2017-09-02 21:08:13 +0200

    added secure pasword to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170902133348) do
+ActiveRecord::Schema.define(version: 20170902135442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170902133348) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
